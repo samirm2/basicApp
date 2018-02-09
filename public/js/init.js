d3ae8e5071c5fad570dedb35799a33fcd1342e1b
Samir Miranda <saanm2@gmail.com>
2018-02-09 00:53:47 -0500

    corrigiendo error al cargar chart.js en produccion

--- public/js/init.js
+++ public/js/init.js
@@ -19,6 +19,14 @@ $(function(){
         'replace': 'Has Clic o Arrastra y Suelta Un Archivo Aqui Para Reemplazar',
         'remove':  'Eliminar',
         'error':   'Ooops, Ha ocurrido un Error.'
-    }
+    	},
+    	error: {
+        'fileSize': 'The file size is too big ({{ value }} max).',
+        'minWidth': 'The image width is too small ({{ value }}}px min).',
+        'maxWidth': 'The image width is too big ({{ value }}}px max).',
+        'minHeight': 'The image height is too small ({{ value }}}px min).',
+        'maxHeight': 'The image height is too big ({{ value }}px max).',
+        'imageFormat': 'El formato de archivo no esta permitido(solo {{ value }}).'
+    	}
 	});
 });
