2b15dba1c05fe299c8d381f13503228b9c21990b
samirm2 <saanm2@gmail.com>
2018-08-11 04:41:10 -0500

    problemas de minusculas en clase detallePqrs

--- app/DetallePqrs.php
+++ app/DetallePqrs.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class DetallePqrs extends Model
+class detallePqrs extends Model
 {
     public function pqrsDatos(){
     	return $this->belongsTo('\App\Pqrs');
